adec75f1254ed6b1e56d07fe9b4d003aa195a379
cswen29 <123640972+cswen29@users.noreply.github.com>
2023-12-12 01:45:11 -0500

    fixed

---
+++ 3DGame/DICUL/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource _source;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        _source.Play();
+    }
+}
